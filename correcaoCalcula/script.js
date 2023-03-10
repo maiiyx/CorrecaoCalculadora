2d63a26e5c4db8d9d6f9686c5c5de104d43cb408
Willyanne Silva <110118532+maiiyx@users.noreply.github.com>
2023-03-10 14:01:42 -0300

    Update script.js

--- correcaoCalcula/script.js
+++ correcaoCalcula/script.js
@@ -1,26 +1,26 @@
-body{
-    background-color: #d9d9d9;
-}
-.calculadora{
-    margin-top: 40px;
-    border: 1px solid #000;
-    padding: 20px;
-    border-radius: 10px;
-    box-shadow: 1px 1px 5px #000;
-    width: 480px;
-}
+//tipo = ação ou valor
+//valor = representação do valor do botão
+function calcular(tipo, valor){
+    // console.log(tipo, valor)
 
-.btn{
-    width: 100px;
-    height: 50px;
-    margin: 5px;
-    box-shadow: 1px 1px 1px #000;
-}
+    if(tipo === 'acao'){
+        // limpar a tela da calculadora
 
-.clear, .zero{
-    width: 210px;
-}
+        if(valor === 'c'){
+            document.getElementById('resultado').value = ''
+        }
 
-.enter{
-    height: 110px;
+        if(valor === '+' || valor === '-' || valor === '*' || valor === '/' || valor === '.'){
+            document.getElementById('resultado').value += valor
+        }
+
+        if(valor === '='){
+            let valor_campo = eval(document.getElementById('resultado').value)
+            document.getElementById('resultado').value = valor_campo
+        }
+
+    } else if(tipo === 'valor'){
+        document.getElementById('resultado').value += valor
+
+    }
 }
